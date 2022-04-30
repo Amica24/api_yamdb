860b2782131900137588c328c6a8e34efbbd51c6
Виктория Мальцева <sidyakova24@gmail.com>
2022-04-30 18:09:05 +0500

    fixed permissions file

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,16 +1,16 @@
-from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
 
-class IsAdminOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_staff)
+                or request.user.is_admin)
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.is_staff)
+                or request.user.is_admin)
 
 
-class IsAdmin(BasePermission):
+class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
